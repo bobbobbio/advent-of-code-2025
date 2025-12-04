122424f42cd9efd3a1bbe80eee1ea7c0b21fdb43
Remi Bernotavicius <remi@abort.cc>
2025-12-03 21:50:31 -0800

    Move Direction to support

--- four/src/main.rs
+++ four/src/main.rs
@@ -8,66 +8,12 @@ enum Cell {
     Roll,
 }
 
-#[derive(EnumIter)]
-enum Direction {
-    North,
-    NorthEast,
-    East,
-    SouthEast,
-    South,
-    SouthWest,
-    West,
-    NorthWest,
-}
-
-impl Direction {
-    fn advance(
-        &self,
-        mut x: usize,
-        mut y: usize,
-        width: usize,
-        height: usize,
-    ) -> Option<(usize, usize)> {
-        match self {
-            Self::North | Self::NorthEast | Self::NorthWest => {
-                if y == 0 {
-                    return None;
-                }
-                y -= 1;
-            }
-            Self::South | Self::SouthEast | Self::SouthWest => {
-                if y >= height - 1 {
-                    return None;
-                }
-                y += 1;
-            }
-            _ => {}
-        }
-        match self {
-            Self::East | Self::NorthEast | Self::SouthEast => {
-                if x >= width - 1 {
-                    return None;
-                }
-                x += 1;
-            }
-            Self::West | Self::NorthWest | Self::SouthWest => {
-                if x == 0 {
-                    return None;
-                }
-                x -= 1;
-            }
-            _ => {}
-        }
-        Some((x, y))
-    }
-}
-
 fn removable_rolls(map: &Grid<Cell>) -> Vec<(usize, usize)> {
     let mut to_remove = vec![];
     for (i, j) in map.positions() {
         if matches!(map[i][j], Cell::Roll) {
             let mut paper_neighbors = 0;
-            for d in Direction::iter() {
+            for d in Direction8::iter() {
                 if let Some((ii, jj)) = d.advance(i, j, map.width(), map.height()) {
                     if matches!(map[ii][jj], Cell::Roll) {
                         paper_neighbors += 1;
